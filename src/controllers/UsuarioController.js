b51b84e248d296672a850057b209ef2413707d13
alexdgoncalves <alex.d.goncalves21@gmail.com>
2022-10-22 22:54:21 -0300

    adicionado restante do crud

--- src/controllers/UsuarioController.js
+++ src/controllers/UsuarioController.js
@@ -9,7 +9,11 @@ class UsuarioController {
 
   static async buscaTodosUsuarios(req, res) {
     try {
-      const todosUsuarios = await database.Usuarios.findAll();
+      const todosUsuarios = await database.Usuarios.findAll({
+        where: {
+          ativo: true
+        }
+      });
       return res.status(200).json(todosUsuarios);
     }
     catch (error) {
@@ -55,6 +59,65 @@ class UsuarioController {
       SmsController.msgBoasVindas();
 
       return res.status(200).json(usuario);
+    }
+    catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async atualizaUsuario(req, res) {
+    const { id } = req.params;
+    const novasInfos = req.body;
+
+    try {
+      await database.Usuarios.update(
+        novasInfos,
+        {
+          where: {
+            id: Number(id)
+          }
+        }
+      );
+
+      const usuarioAtualizado = await database.Usuarios.findOne({
+        where: {
+          id: Number(id)
+        }
+      });
+
+      return res.status(200).json(usuarioAtualizado);
+    }
+    catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async desativaUsuario(req, res) {
+    const { id } = req.params;
+
+    try {
+      await database.Usuarios.update(
+        { ativo: false },
+        {
+          where: {
+            id: Number(id)
+          }
+        }
+      );
+      return res.status(200).json({ mensagem: `id ${id} desativado` });
+    } catch (error) {
+      return res.status(500).json(error.message);
+    }
+  }
+
+  static async buscaTodosUsuariosDesativados(req, res) {
+    try {
+      const todosUsuariosDesativados = await database.Usuarios.findAll({
+        where: {
+          ativo: false
+        }
+      });
+      return res.status(200).json(todosUsuariosDesativados);
     }
     catch (error) {
       return res.status(500).json(error.message);
--- src/routes/usuariosRoute.js
+++ src/routes/usuariosRoute.js
@@ -1,11 +1,13 @@
 const { Router } = require('express');
-const bcrypt = require("bcryptjs");
 const UsuarioController = require('../controllers/UsuarioController');
 
 const router = Router();
 
 router.get('/usuarios', UsuarioController.buscaTodosUsuarios);
+router.get('/usuariosDesativados', UsuarioController.buscaTodosUsuariosDesativados);
 router.post('/usuarios', UsuarioController.criaUsuario);
+router.put('/usuarios/:id', UsuarioController.atualizaUsuario);
+router.put('/desativaUsuarios/:id', UsuarioController.desativaUsuario);
 
 
 module.exports = router;
